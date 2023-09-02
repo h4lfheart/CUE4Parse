a904df0e2cadae4ebc7ace1cc3f3352cfc0d11df
halfuwu <maxman1346@gmail.com>
2023-09-02 13:07:15 -0400

    compression descriptions for fmodel

--- CUE4Parse-Conversion/UEFormat/EFileCompressionFormat.cs
+++ CUE4Parse-Conversion/UEFormat/EFileCompressionFormat.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel;
+
 namespace CUE4Parse_Conversion.UEFormat;
 
 public enum EFileCompressionFormat
 {
+    [Description("Uncompressed")]
     None,
+    
+    [Description("Gzip Compression")]
     GZIP,
+    
+    [Description("ZStandard Compression")]
     ZSTD
 }
