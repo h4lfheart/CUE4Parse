c318580dfc4fbeb43cdec6dfc14278dfa307ab8d
halfuwu <maxman1346@gmail.com>
2024-06-12 00:27:39 -0400

    fix skeletal meshes for 27.11

--- CUE4Parse/UE4/Assets/Exports/SkeletalMesh/FStaticLODModel.cs
+++ CUE4Parse/UE4/Assets/Exports/SkeletalMesh/FStaticLODModel.cs
@@ -407,7 +407,7 @@ namespace CUE4Parse.UE4.Assets.Exports.SkeletalMesh
                 }
             }
 
-            if (FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.SkeletalHalfEdgeData)
+            if (FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.SkeletalHalfEdgeData && Ar.Game is not EGame.GAME_Fortnite_27_11)
             {
                 const byte MeshDeformerStripFlag = 1;
                 var meshDeformerStripFlags = Ar.Read<FStripDataFlags>();
