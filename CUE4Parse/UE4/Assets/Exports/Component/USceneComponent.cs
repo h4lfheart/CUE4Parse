add555807fac5abd8228c01f366a8bfbb0801a87
halfuwu <maxman1346@gmail.com>
2024-08-12 19:55:11 -0400

    fix scene component default scale

--- CUE4Parse/UE4/Assets/Exports/Component/USceneComponent.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/USceneComponent.cs
@@ -1,6 +1,20 @@
-namespace CUE4Parse.UE4.Assets.Exports.Component
+using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Core.Math;
+
+namespace CUE4Parse.UE4.Assets.Exports.Component;
+
+public class USceneComponent : UObject
 {
-    public class USceneComponent : UObject
+    public FVector RelativeLocation;
+    public FRotator RelativeRotation;
+    public FVector RelativeScale3D;
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
     {
+        base.Deserialize(Ar, validPos);
+
+        RelativeLocation = GetOrDefault(nameof(RelativeLocation), FVector.ZeroVector);
+        RelativeRotation = GetOrDefault(nameof(RelativeRotation), FRotator.ZeroRotator);
+        RelativeScale3D = GetOrDefault(nameof(RelativeScale3D), FVector.OneVector);
     }
 }
