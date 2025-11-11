7f8a78faf6450f514017a76a3f23fec06dc2a0b3
halfuwu <maxman1346@gmail.com>
2025-11-11 13:17:52 -0500

    fortnite s27 fixes

--- CUE4Parse/UE4/Assets/Exports/SkeletalMesh/FStaticLODModel.cs
+++ CUE4Parse/UE4/Assets/Exports/SkeletalMesh/FStaticLODModel.cs
@@ -544,7 +544,7 @@ public class FStaticLODModel
             }
         }
 
-        if (FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.SkeletalHalfEdgeData)
+        if (FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.SkeletalHalfEdgeData && Ar.Game is not EGame.GAME_Fortnite_S27)
         {
             const byte MeshDeformerStripFlag = 1;
             var meshDeformerStripFlags = Ar.Read<FStripDataFlags>();
